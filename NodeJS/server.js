0b9e412773052f248ae83ece695d9a1369aad894
Tomgacutara <rustom.gacutra@gmail.com>
2024-11-07 00:24:04 +0800

    update

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -9,10 +9,8 @@ const PORT = 3000;
 app.use(express.json());
 
 app.use(cors({
-    origin: ['http://127.0.0.1:5501', 'http://localhost:5501']
-  }));
-  
-
+    origin: 'https://tomgacutara.github.io'
+}));
 
 // Login route (original)
 app.post('/login', (req, res) => {
